44cdd385e9f2ba1a9acb65b6f5e8b7efa5f39793
Alex2890 <angelagrouchybuhayko04@gmail.com>
2024-02-03 20:59:11 +0800

    changed wishlist icon and fill to red when hover

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,114 +1,112 @@
-import React from "react";
-import { Link } from "react-router-dom"
-import { useContext } from "react";
-import { allData } from "../context/AppContext.js";
-import Cookies from 'js-cookie';
-
-function Navbar() {
-
-  const { user, setUser } = useContext(allData)
-  console.log(user)
-
-  const logoutHandler = () => {
-
-    localStorage.removeItem('user');
-    window.location.href = "/";
-  }
-
-
-
-  return (
-    <div className="navbar py-3">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </div>
-          <ul tabIndex={0} className="text-gray-500 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/aboutus'>About Us</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact</Link>
-            </li>
-            <li>
-              <Link to='/products'>Products</Link>
-            </li>
-            <li>
-              <Link to='/faqs'>FAQs</Link>
-            </li>
-            <li>
-              <Link to='/upload'>Upload Admin</Link>
-            </li>
-            <li>
-              <Link to='/cart'>Cart</Link>
-            </li>
-
-          </ul>
-        </div>
-        <Link to='/'><p className="btn btn-ghost text-lg">LuxuriaLoom</p></Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-gray-500">
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/aboutus'>About Us</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-          <li>
-            <Link to='/products'>Products</Link>
-          </li>
-          <li>
-            <Link to='/faqs'>FAQs</Link>
-          </li>
-          <li>
-            <Link to='/upload'>Upload Admin(will remove)</Link>
-          </li>
-          <li>
-            <Link to='/cart'>Cart</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <div>
-
-          <div className="dropdown dropdown-bottom dropdown-end">
-            <div tabIndex={0} role="button" className="btn m-1">
-              {user?.user.firstName}
-              <div className="ms-1 avatar placeholder">
-                <div className="bg-neutral text-neutral-content rounded-full w-8">
-                  <span className="text-xs">UI</span>
-                </div>
-              </div>
-            </div>
-            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-              <li>
-                {user?.user.firstName}
-
-              </li>
-              <li>
-                {user ? <button onClick={logoutHandler} className="btn btn-ghost text-gray-500"><Link to=''>Logout</Link></button> : <button className="btn btn-ghost text-gray-500"><Link to='/login'>Login</Link></button>
-                }
-              </li>
-
-            </ul>
-          </div>
-
-
-        </div>
-
-      </div>
-    </div>
-
-  );
-}
-
-
+import React from "react"; 
+import { Link } from "react-router-dom" 
+import { useContext } from "react"; 
+import { allData } from "../context/AppContext.js"; 
+import Cookies from 'js-cookie'; 
+ 
+function Navbar() { 
+ 
+  const { user, setUser } = useContext(allData) 
+  console.log(user) 
+ 
+  const logoutHandler = () => { 
+ 
+    localStorage.removeItem('user'); 
+    window.location.href = "/"; 
+  } 
+ 
+ 
+ 
+  return ( 
+    <div className="navbar py-3"> 
+      <div className="navbar-start"> 
+        <div className="dropdown"> 
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden"> 
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg> 
+          </div> 
+          <ul tabIndex={0} className="text-gray-500 menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"> 
+            <li> 
+              <Link to='/'>Home</Link> 
+            </li> 
+            <li> 
+              <Link to='/aboutus'>About Us</Link> 
+            </li> 
+            <li> 
+              <Link to='/contact'>Contact</Link> 
+            </li> 
+            <li> 
+              <Link to='/products'>Products</Link> 
+            </li> 
+            <li> 
+              <Link to='/faqs'>FAQs</Link> 
+            </li> 
+            <li> 
+              <Link to='/upload'>Upload Admin</Link> 
+            </li> 
+            <li> 
+              <Link to='/cart'>Cart</Link> 
+            </li> 
+ 
+          </ul> 
+        </div> 
+        <Link to='/'><p className="btn btn-ghost text-lg">LuxuriaLoom</p></Link> 
+      </div> 
+      <div className="navbar-center hidden lg:flex"> 
+        <ul className="menu menu-horizontal px-1 text-gray-500"> 
+          <li> 
+            <Link to='/'>Home</Link> 
+          </li> 
+          <li> 
+            <Link to='/aboutus'>About Us</Link> 
+          </li> 
+          <li> 
+            <Link to='/contact'>Contact</Link> 
+          </li> 
+          <li> 
+            <Link to='/products'>Products</Link> 
+          </li> 
+          <li> 
+            <Link to='/faqs'>FAQs</Link> 
+          </li> 
+          <li> 
+            <Link to='/upload'>Upload Admin(will remove)</Link> 
+          </li> 
+          <li> 
+            <Link to='/cart'>Cart</Link> 
+          </li> 
+        </ul> 
+      </div> 
+      <div className="navbar-end"> 
+        <div> 
+ 
+          <div className="dropdown dropdown-bottom dropdown-end"> 
+            <div tabIndex={0} role="button" className="btn m-1"> 
+              {user?.user.firstName} 
+              <div className="ms-1 avatar placeholder"> 
+                <div className="bg-neutral text-neutral-content rounded-full w-8"> 
+                  <span className="text-xs">UI</span> 
+                </div> 
+              </div> 
+            </div> 
+            <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"> 
+              <li> 
+                {user?.user.firstName} 
+ 
+              </li> 
+              <li> 
+                {user ? <button onClick={logoutHandler} className="btn btn-ghost text-gray-500"><Link to=''>Logout</Link></button> : <button className="btn btn-ghost text-gray-500"><Link to='/login'>Login</Link></button> 
+                } 
+              </li> 
+ 
+            </ul> 
+          </div> 
+        </div> 
+ 
+      </div> 
+    </div> 
+ 
+  ); 
+} 
+ 
+ 
 export default Navbar
--- frontend/src/pages/SingleProduct.js
+++ frontend/src/pages/SingleProduct.js
@@ -128,8 +128,12 @@ const SingleProduct = () => {
                                             <div className="ml-2 sm:ml-8">
                                                 <button className="bg-black leading-none py-4 px-5 md:px-8 font-normal text-sm h-11 text-white transition-all hover:bg-orange">Add to Cart</button>
                                             </div>
-                                            <a href="#" className="text-md ml-8"><i className="hover:text-red-700">{heart}</i></a>
-                                            <a href="#" className="text-md ml-8"><i className="icon-refresh"></i></a>
+                                            <Link to="/wishlist" className="text-md ml-8">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:fill-red-700 hover:stroke-red-700">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                                </svg>
+                                            </Link>
+                                            {/* <Link to="/" className="text-md ml-8"><i className="icon-refresh"></i></Link> */}
                                         </div>
                                     </div>
 
